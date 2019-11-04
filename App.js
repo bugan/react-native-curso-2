5492d06c1bc56f60089614976909631392f99171
Ricardo Bugan <dones.bugan@hotmail.com>
2019-11-04 18:06:03 -0300

    video 3.1

--- App.js
+++ App.js
@@ -1,7 +1,19 @@
 import React from "react";
 import Feed from "./src/Views/Feed/Feed";
 import Login from "./src/Views/Login/Login";
+
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+
+const AppNavigator = createStackNavigator({
+  Login: {
+    screen: Login
+  }
+});
+const AppConteiner = createAppContainer(AppNavigator);
+
 const App = () => {
-  return <Login />;
+  return <AppConteiner />;
 };
+
 export default App;
